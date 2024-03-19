2f18eaed257cdb905355b7fade9cce48b762123c
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-03-19 15:50:28 +0100

    Tech Capabilities JavaDoc Added #2

--- src/main/java/org/folio/roles/integration/kafka/CapabilityEventProcessor.java
+++ src/main/java/org/folio/roles/integration/kafka/CapabilityEventProcessor.java
@@ -76,7 +76,8 @@ public class CapabilityEventProcessor {
    * It is needed to support "FOLIO PermissionSets" using plain Eureka CapabilitySets.
    * The Capabilities created to reflect the CapabilitySets are called "Technical Capabilities" and do not refer
    * to any actual permissions/access grants that can be available to users.
-   * Such Capabilities can be distinguished by the following criteria: the end-points list of the capability must be empty,
+   * Such Capabilities can be distinguished by the following criteria:
+   * the end-points list of the capability must be empty,
    * and the capability name must be equal to the capability set name that it reflects.
    */
   private CapabilityResultHolder processUiModuleResources(CapabilityEvent event, List<FolioResource> resources) {
