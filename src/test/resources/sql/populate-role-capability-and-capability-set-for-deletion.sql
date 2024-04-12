09fabb45e4b108624dcbb3c621ee091e8aec605b
yaroslav-kiriak <159446370+yaroslav-kiriak@users.noreply.github.com>
2024-04-12 15:00:38 +0300

    [MODROLESKC-157]: add onDelete cascade constraint to role_capability_set with role association (#68)
    Co-authored-by: Yaroslav_Kiriak <Yaroslav_Kiriak@epam.com>

--- src/test/resources/sql/populate-role-capability-and-capability-set-for-deletion.sql
+++ src/test/resources/sql/populate-role-capability-and-capability-set-for-deletion.sql
@@ -17,3 +17,13 @@ VALUES ('8d2da27c-1d56-48b6-958d-2bfae6d79dc8', 'foo_item.create',
 INSERT INTO test_mod_roles_keycloak.role_capability(role_id, capability_id)
 VALUES ('1e985e76-e9ca-401c-ad8e-0d121a11111e', '8d2da27c-1d56-48b6-958d-2bfae6d79dc8'),
        ('1e985e76-e9ca-401c-ad8e-0d121a11111e', 'e2628d7d-059a-46a1-a5ea-10a5a37b1af2');
+
+INSERT INTO test_mod_roles_keycloak.capability_set
+  (id, name, description, resource, action, type, application_id, created_by)
+VALUES ('8d2da27c-1d56-48b6-958d-2bfae6d79dc8', 'foo_item.create',
+        'Capability to create a foo item', 'Foo Item', 'CREATE', 'DATA', 'foo-application-1.0.0',
+        '11111111-1111-4011-1111-0d121a11111e');
+
+INSERT INTO test_mod_roles_keycloak.role_capability_set (role_id, capability_set_id, created_by, created_date)
+VALUES ('1e985e76-e9ca-401c-ad8e-0d121a11111e', '8d2da27c-1d56-48b6-958d-2bfae6d79dc8',
+        '11111111-2222-1111-2222-111111111111', '2023-09-05 15:00:00');
