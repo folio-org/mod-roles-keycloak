38da3c7dfdb3c6f8e67423b488f4dd8ad43f0c6d
saba_zedginidze <saba_zedginidze@epam.com>
2024-08-01 12:44:58 +0400

    Test

--- src/test/java/org/folio/roles/service/policy/PolicyEntityServiceTest.java
+++ src/test/java/org/folio/roles/service/policy/PolicyEntityServiceTest.java
@@ -84,7 +84,6 @@ class PolicyEntityServiceTest {
 
       var policy = service.create(timePolicy);
 
-
       assertEquals(timePolicy.getId(), policy.getId());
       assertEquals(timePolicy.getTimePolicy(), policy.getTimePolicy());
       assertEquals(PolicyType.TIME, policy.getType());
