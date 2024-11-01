e3a395f92cdacc6746f2c00cbd4b329cf0a29643
Mykola Makhin <mmakhin@ebsco.com>
2024-11-01 10:59:14 +0200

    MODROLESKC-214: Use systemUserScopedExecutionService.executeSystemUserScoped for populating folio execution context on Kafka message handling.

--- src/main/java/org/folio/roles/integration/kafka/KafkaMessageListener.java
+++ src/main/java/org/folio/roles/integration/kafka/KafkaMessageListener.java
@@ -5,7 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.folio.roles.integration.kafka.model.ResourceEvent;
 import org.folio.roles.service.capability.CapabilityReplacementsService;
 import org.folio.spring.context.ExecutionContextBuilder;
-import org.folio.spring.scope.FolioExecutionContextSetter;
+import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +17,7 @@ public class KafkaMessageListener {
   private final ExecutionContextBuilder executionContextBuilder;
   private final CapabilityKafkaEventHandler capabilityKafkaEventHandler;
   private final CapabilityReplacementsService capabilityReplacementsService;
+  private final SystemUserScopedExecutionService systemUserScopedExecutionService;
 
   /**
    * Handles capability event.
@@ -29,10 +30,11 @@ public class KafkaMessageListener {
     groupId = "#{folioKafkaProperties.listener['capability'].groupId}",
     topicPattern = "#{folioKafkaProperties.listener['capability'].topicPattern}")
   public void handleCapabilityEvent(ResourceEvent resourceEvent) {
-    try (
-      var ignored = new FolioExecutionContextSetter(executionContextBuilder.buildContext(resourceEvent.getTenant()))) {
+    systemUserScopedExecutionService.executeSystemUserScoped(() -> {
       var capabilityReplacements = capabilityKafkaEventHandler.handleEvent(resourceEvent);
       capabilityReplacements.ifPresent(capabilityReplacementsService::processReplacements);
-    }
+      return null;
+    });
   }
 }
+
--- src/test/java/org/folio/roles/integration/kafka/KafkaMessageListenerTest.java
+++ src/test/java/org/folio/roles/integration/kafka/KafkaMessageListenerTest.java
@@ -3,18 +3,17 @@ package org.folio.roles.integration.kafka;
 import static org.folio.roles.support.CapabilityUtils.APPLICATION_ID;
 import static org.folio.roles.support.TestConstants.TENANT_ID;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import org.folio.roles.integration.kafka.model.ResourceEvent;
 import org.folio.roles.service.capability.CapabilityReplacementsService;
-import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
-import org.folio.spring.context.ExecutionContextBuilder;
+import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.folio.test.types.UnitTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -33,7 +32,7 @@ class KafkaMessageListenerTest {
   @Mock private FolioModuleMetadata folioModuleMetadata;
   @Mock private CapabilityKafkaEventHandler capabilityKafkaEventHandler;
   @Mock private CapabilityReplacementsService capabilityReplacementsService;
-  @Mock private ExecutionContextBuilder executionContextBuilder;
+  @Mock private SystemUserScopedExecutionService systemUserScopedExecutionService;
 
   @AfterEach
   void tearDown() {
@@ -42,7 +41,11 @@ class KafkaMessageListenerTest {
 
   @Test
   void handleCapabilityEvent_positive() {
-    when(executionContextBuilder.buildContext(any())).thenReturn(mock(FolioExecutionContext.class));
+    when(systemUserScopedExecutionService.executeSystemUserScoped(any())).thenAnswer(inv -> {
+      Callable<?> callable = inv.getArgument(0);
+      callable.call();
+      return null;
+    });
     var resourceEvent = ResourceEvent.builder()
       .tenant(TENANT_ID)
       .newValue(capabilityEventBodyAsMap())
