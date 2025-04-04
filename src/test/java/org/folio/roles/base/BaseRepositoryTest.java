3b3ca9e17c2ebc954479a9fbef3b1a05fff9aaa9
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2025-04-04 18:17:35 +0300

    MODROLESKC-288 - return afterAll from BaseRepositoryTest

--- src/test/java/org/folio/roles/base/BaseRepositoryTest.java
+++ src/test/java/org/folio/roles/base/BaseRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.folio.roles.base;
 
+import static org.folio.test.TestUtils.readString;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
 
 import lombok.extern.log4j.Log4j2;
@@ -9,11 +10,13 @@ import org.folio.spring.cql.JpaCqlConfiguration;
 import org.folio.test.extensions.EnablePostgres;
 import org.folio.test.extensions.LogTestMethod;
 import org.folio.test.types.IntegrationTest;
+import org.junit.jupiter.api.AfterAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -29,4 +32,9 @@ public abstract class BaseRepositoryTest {
 
   @Autowired protected TestEntityManager entityManager;
   @MockitoBean protected FolioExecutionContext folioExecutionContext;
+
+  @AfterAll
+  static void afterAll(@Autowired JdbcTemplate jdbcTemplate) {
+    jdbcTemplate.execute(readString("sql/drop-tables.sql"));
+  }
 }
