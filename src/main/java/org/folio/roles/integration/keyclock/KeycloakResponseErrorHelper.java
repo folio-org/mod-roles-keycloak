4a9734cc86024846f92c24dd15747a20949f4876
Vavilkin <yauhen_vavilkin@epam.com>
2026-05-12 19:18:56 +0400

    fix(logging): add missing Keycloak response error helper

---
+++ src/main/java/org/folio/roles/integration/keyclock/KeycloakResponseErrorHelper.java
@@ -0,0 +1,36 @@
+package org.folio.roles.integration.keyclock;
+
+import static java.lang.String.format;
+
+import jakarta.ws.rs.core.Response;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class KeycloakResponseErrorHelper {
+
+  private static final int MAX_RESPONSE_BODY_LENGTH = 2048;
+
+  static String errorDetails(Response response) {
+    var statusInfo = response.getStatusInfo();
+    var details = format("status = %s, message = %s", statusInfo.getStatusCode(), statusInfo.getReasonPhrase());
+    var responseBody = responseBody(response);
+    return responseBody == null ? details : details + ", responseBody = " + responseBody;
+  }
+
+  private static String responseBody(Response response) {
+    try {
+      if (!response.hasEntity()) {
+        return null;
+      }
+
+      var responseBody = response.readEntity(String.class);
+      return StringUtils.isBlank(responseBody)
+        ? null
+        : StringUtils.abbreviate(StringUtils.normalizeSpace(responseBody), MAX_RESPONSE_BODY_LENGTH);
+    } catch (RuntimeException exception) {
+      return format("unavailable: %s: %s", exception.getClass().getSimpleName(), exception.getMessage());
+    }
+  }
+}
